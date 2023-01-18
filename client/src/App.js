669ecd964e7d65121149020f5e1e492a7fdc47d8
[Andres Perez] <mandresperez.uk@gmail.com>
2023-01-18 10:12:26 -0700

    Added in components with limited functionality, still need to add HTML on most js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import About from "./components/About/About";
+import Donate from "./components/Donate/Donate";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
+  const [currentPage, setCurrentPage] = useState('Home');
 
+  const renderPage = () => {
+    if (currentPage === 'Login') {
+      return <Login />;
+    }
+    if (currentPage === 'Signup') {
+      return <Signup />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Donate') {
+      return <Donate />;
+    } else {
+      return <Home />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
   );
 }
 
---
+++ client/src/components/Donate/Donate.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css';
+
+export default function Donate() {
+    return (
+        <div className="container">
+            <h1>
+                "ALONE WE CAN DO SO LITTLE; TOGETHER, WE CAN DO SO MUCH."
+            </h1>
+            <h2>
+                HOWDYNEIGHBOR IS COMMUNITY DRIVEN & BACKED BY CHARITABLE DONATIONS.
+            </h2>
+
+
+        </div>
+    )
+}
