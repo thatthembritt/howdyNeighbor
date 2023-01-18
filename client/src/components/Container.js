5a10f5774f7e347701dcd318b4980dc488d4d678
[Andres Perez] <mandresperez.uk@gmail.com>
2023-01-17 17:22:59 -0700

    Laid out basic file structure for components, they still need html once we lay out the template html we can copy over

---
+++ client/src/components/Container.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar/Navbar';
+import Home from './Home/Home';
+import About from './About/About';
+import Portfolio from './Portfolio/Portfolio';
+import Form from './Form/index';
+import Resume from './Resume/Resume';
+
+function Container() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Form />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
+
+export default Container;
