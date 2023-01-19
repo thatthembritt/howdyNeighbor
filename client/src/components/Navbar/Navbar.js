29fb2cc2800d73c27de1d33408582caa67183217
[Andres Perez] <mandresperez.uk@gmail.com>
2023-01-19 16:05:22 -0700

    Added logo to top of page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,43 +3,46 @@ import './style.css';
 
 export default function Navbar({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="/login"
-          onClick={() => handlePageChange('Login')}
-          className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
-        >
-          Login
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="/signup"
-          onClick={() => handlePageChange('Signup')}
-          className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
-        >
-          Sign Up
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="/about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About Us
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="/donate"
-          onClick={() => handlePageChange('Donate')}
-          className={currentPage === 'Donate' ? 'nav-link active' : 'nav-link'}
-        >
-          Donate
-        </a>
-      </li>
-    </ul>
+    <div className="container">
+      <img src="./howdyneighbor_4.png"></img>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="/login"
+            onClick={() => handlePageChange('Login')}
+            className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
+          >
+            Login
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="/signup"
+            onClick={() => handlePageChange('Signup')}
+            className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}
+          >
+            Sign Up
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="/about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About Us
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="/donate"
+            onClick={() => handlePageChange('Donate')}
+            className={currentPage === 'Donate' ? 'nav-link active' : 'nav-link'}
+          >
+            Donate
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
