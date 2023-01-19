9af4c4dd99b3322a29a7dce89150baf7128774e4
Brittany Boyd <brttnybyd@gmail.com>
2023-01-19 10:43:45 -0700

    updated login.js and signup.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,8 +1,110 @@
-import React from 'react'
-import './style.css';
+import React from "react";
+import "./index.css";
+import {
+  AlignCenterOutlined,
+  BorderOuterOutlined,
+  LockOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
 
-export default function Login() {
-    return (
+const contentStyle = {
+  background: "#B3D0B3",
+  border: "20px",
+  borderRadius: "5px",
+  paddingBottom: "20px",
+  paddingTop: "10px",
+};
 
-    )
-}
+const buttonStyle = {
+  background: "#fcc931",
+  color: "#252827",
+  marginBottom: "40px",
+  width: "200px",
+  marginLeft: "40px",
+};
+
+const linkStyle = {
+  color: "#252827",
+  paddingLeft: "45px",
+};
+
+const inputStyle = {
+  color: "#fcc931",
+};
+
+const userNameStyle = {
+  padding: "20px",
+  marginBottom: "0px",
+};
+
+const passwordStyle = {
+  marginTop: "0px",
+  padding: "20px",
+};
+
+const App = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <Form
+      style={contentStyle}
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        style={userNameStyle}
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Username!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="USERNAME"
+          style={inputStyle}
+        />
+      </Form.Item>
+      <Form.Item
+        style={passwordStyle}
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Password!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="PASSWORD"
+          style={inputStyle}
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="login-form-button"
+          style={buttonStyle}
+        >
+          SIGN IN
+        </Button>
+        <a href="" style={linkStyle}>
+          NEW TO THE APP? SIGN UP HERE
+        </a>
+      </Form.Item>
+    </Form>
+  );
+};
+export default App;
--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,7 +1,80 @@
-import React from 'react';
+import React from "react";
 
-export default function Signup() {
+import React from "react";
+import "./index.css";
+import { Button, Form, Input, InputNumber } from "antd";
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const contentStyle = {
+  background: "#B3D0B3",
+};
+
+const buttonStyle = {
+  background: "#fcc931",
+  color: "#252827",
+  marginBottom: "40px",
+  width: "200px",
+  marginLeft: "40px",
+};
+
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+/* eslint-enable no-template-curly-in-string */
+
+const App = () => {
+  const onFinish = (values) => {
+    console.log(values);
+  };
   return (
-    
+    <Form
+      {...layout}
+      name="nest-messages"
+      onFinish={onFinish}
+      validateMessages={validateMessages}
+      style={contentStyle}
+    >
+      <Form.Item name={["user", "firstname"]} label="FIRST NAME">
+        <Input />
+      </Form.Item>
+      <Form.Item name={["user", "lastname"]} label="LAST NAME">
+        <Input />
+      </Form.Item>
+      <Form.Item name={["user", "zipcode"]} label="ZIPCODE">
+        <Input />
+      </Form.Item>
+      <Form.Item name={["user", "email"]} label="EMAIL">
+        <Input />
+      </Form.Item>
+      <Form.Item name={["user", "password"]} label="PASSWORD">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        wrapperCol={{
+          ...layout.wrapperCol,
+          offset: 8,
+        }}
+      >
+        <Button type="primary" htmlType="submit" style={buttonStyle}>
+          SIGN UP
+        </Button>
+      </Form.Item>
+    </Form>
   );
-}
+};
+export default App;
