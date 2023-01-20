d06c7514157596b5dbff2c465a9d874de4b19349
Charles Dykstra <charles.b.dykstra@gmail.com>
2023-01-19 21:08:37 -0700

    redid the login and removed the antd, as this was causeing a problem with the submit.  Re did the singup, page.  set the mutations file in utils to match

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,51 +1,13 @@
-import React, { useEffect, useState } from "react";
-import "./style.css";
-import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Alert } from "antd";
+import React, { useState, useEffect } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../../utils/mutations";
 
 import Auth from "../../utils/auth";
 
-
-const contentStyle = {
-  background: "#B3D0B3",
-  border: "20px",
-  borderRadius: "5px",
-  paddingBottom: "20px",
-  paddingTop: "10px",
-};
-
-const buttonStyle = {
-  background: "#fcc931",
-  color: "#252827",
-  marginBottom: "40px",
-  width: "200px",
-  marginLeft: "40px",
-};
-
-const linkStyle = {
-  color: "#252827",
-  paddingLeft: "45px",
-};
-
-const inputStyle = {
-  color: "#fcc931",
-};
-
-const userNameStyle = {
-  padding: "20px",
-  marginBottom: "0px",
-};
-
-const passwordStyle = {
-  marginTop: "0px",
-  padding: "20px",
-};
-
-
 const Login = () => {
-  const [userFormData, setUserFormData] = useState({ userName: "", password: "" });
+  const [userFormData, setUserFormData] = useState({ username: "", password: "" });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -66,9 +28,7 @@ const Login = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    //console.log(userFormData)
 
-    // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -82,8 +42,8 @@ const Login = () => {
 
       console.log(data);
       Auth.login(data.login.token);
-    } catch (err) {
-      console.error(err);
+    } catch (e) {
+      console.error(e);
     }
 
     // clear form values
@@ -93,87 +53,56 @@ const Login = () => {
     });
   };
 
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
-  };
   return (
-    <Form
-      noValidate
-      validated={validated}
-      onSubmit={handleFormSubmit}
-      style={contentStyle}
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Alert
-        dismissible
-        onClose={() => setShowAlert(false)}
-        show={showAlert}
-        variant="danger"
-      >
-        Something went wrong with your login credentials!
-      </Alert>
-      <Form.Item
-        style={userNameStyle}
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Username!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          type="text"
-          placeholder="USERNAME"
-          name="username"
-          onChange={handleInputChange}
-          value={userFormData.username}
-          style={inputStyle}
-        />
-      </Form.Item>
-      <Form.Item
-        style={passwordStyle}
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="PASSWORD"
-          onChange={handleInputChange}
-          value={userFormData.password}
-          required
-          style={inputStyle}
-        />
-      </Form.Item>
+    <>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert
+          dismissible
+          onClose={() => setShowAlert(false)}
+          show={showAlert}
+          variant="danger"
+        >
+          Something went wrong with your login credentials!
+        </Alert>
+        <Form.Group>
+          <Form.Label htmlFor="username">username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your username"
+            name="username"
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Username is required!
+          </Form.Control.Feedback>
+        </Form.Group>
 
-      <Form.Item>
+        <Form.Group>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            name="password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
+        </Form.Group>
         <Button
           disabled={!(userFormData.username && userFormData.password)}
-          type="primary"
-          htmlType="submit"
+          type="submit"
           variant="success"
-          className="login-form-button"
-          style={buttonStyle}
         >
-          SIGN IN
+          Submit
         </Button>
-        <a href="#signup" style={linkStyle}>
-          NEW TO THE APP? SIGN UP HERE
-        </a>
-      </Form.Item>
-    </Form>
+      </Form>
+    </>
   );
 };
+
 export default Login;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,7 @@ const {gql} = require ("apollo-server-express")
 
 const typeDefs = gql`
   type User {
+    _id: ID
     username: String
     email: String
     password: String
@@ -33,7 +34,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String, password: String): Auth
+    login(username: String, password: String): Auth
     addUser(
       username: String
       email: String
